2d0050cd49347b5055f13279498da239864f9545
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-08-02 16:05:49 +0900

    kjh view adapter

--- app/src/main/java/com/rajkumarrajan/mvvm_architecture/ui/main/adapter/MainAdapter.kt
+++ app/src/main/java/com/rajkumarrajan/mvvm_architecture/ui/main/adapter/MainAdapter.kt
@@ -1,28 +1,38 @@
 package com.rajkumarrajan.mvvm_architecture.ui.main.adapter
 
 import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.rajkumarrajan.mvvm_architecture.R
 import com.rajkumarrajan.mvvm_architecture.data.model.App
+import com.rajkumarrajan.mvvm_architecture.data.model.FileInfoDto
 import com.rajkumarrajan.mvvm_architecture.data.model.User
 import com.rajkumarrajan.mvvm_architecture.ui.main.view.DetailActivity
+import kotlinx.android.synthetic.main.activity_detail.view.*
 import kotlinx.android.synthetic.main.item_layout.view.*
+import retrofit2.http.Url
 import javax.inject.Inject
 
 class MainAdapter @Inject constructor(
 ) : RecyclerView.Adapter<MainAdapter.DataViewHolder>() {
 
     private var apps: ArrayList<App> = ArrayList()
+    private var files: ArrayList<FileInfoDto> = ArrayList()
 
     class DataViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(app: App) {
+        fun bind(app: App, file: FileInfoDto) {
             itemView.textViewUserName.text = app.appName
+            val imgUrl =  "http://ec2-43-200-14-78.ap-northeast-2.compute.amazonaws.com:8000/file-service/file/download/" +
+                    file.location + "/"+file.changedName
+            Glide.with(itemView).load(imgUrl).into(itemView.imageViewIcon)
+
             itemView.textViewUserEmail.text = app.version
 
         }
@@ -40,13 +50,13 @@ class MainAdapter @Inject constructor(
 
     //user data List binding
     override fun onBindViewHolder(holder: DataViewHolder, position: Int) {
-        holder.bind(apps[position])
+        holder.bind(apps[position], files[position])
 
         //클릭시 user detail page로 이동
         holder.itemView.setOnClickListener{
             val intent = Intent(holder.itemView?.context, DetailActivity::class.java)
             // intent로 id값 전달
-            intent.putExtra("id", position+1)
+            intent.putExtra("id", "2")
             ContextCompat.startActivity(holder.itemView.context, intent, null )
         }
     }
@@ -60,3 +70,4 @@ class MainAdapter @Inject constructor(
     }
 
 }
+
--- app/src/main/java/com/rajkumarrajan/mvvm_architecture/ui/main/view/DetailActivity.kt
+++ app/src/main/java/com/rajkumarrajan/mvvm_architecture/ui/main/view/DetailActivity.kt
@@ -1,6 +1,7 @@
 package com.rajkumarrajan.mvvm_architecture.ui.main.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 
@@ -8,41 +9,25 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import com.rajkumarrajan.mvvm_architecture.R
+import com.rajkumarrajan.mvvm_architecture.databinding.ActivityDetailBinding
 import com.rajkumarrajan.mvvm_architecture.ui.main.viewmodel.DetailViewModel
 import com.rajkumarrajan.mvvm_architecture.utils.Status
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 @AndroidEntryPoint
 class DetailActivity : AppCompatActivity() {
     private val detailViewModel: DetailViewModel by viewModels()
-
-
+    private lateinit var binding : ActivityDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // 넘겨준 Id값 받아서 detail data call
-        val getid = getIntent().getIntExtra("id", 1)
-        setupAPIcall(getid)
 
-    }
-
-    //DetailView Model observe
-    private fun setupAPIcall(testInt: Int){
-        detailViewModel.fetchUserId(testInt).observe(this, Observer {
-            when (it.status) {
-                Status.SUCCESS -> {
-                    textView5.text = it.data.toString()
-                }
-                Status.ERROR -> {
-                    Toast.makeText(this, it.message, Toast.LENGTH_LONG).show()
-                }
-            }
-
-        })
     }
 
 }
