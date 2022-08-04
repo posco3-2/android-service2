e1ee2aeb8883f0037d85660689722b65c3aedfa5
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-08-04 11:32:40 +0900

    [fix] DetailView

--- app/src/main/java/com/posco/posco_store/ui/main/adapter/MainAdapter.kt
+++ app/src/main/java/com/posco/posco_store/ui/main/adapter/MainAdapter.kt
@@ -1,16 +1,14 @@
 package com.posco.posco_store.ui.main.adapter
 
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-
+import android.content.ClipData
 import android.view.LayoutInflater
 
 import android.view.ViewGroup
 import android.widget.Filter
+import android.widget.Filterable
 
 
-import androidx.core.content.ContextCompat.startActivity
+
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.posco.posco_store.R
@@ -18,25 +16,27 @@ import com.posco.posco_store.data.model.App
 import com.posco.posco_store.data.model.FileInfoDto
 import com.posco.posco_store.databinding.ItemLayoutBinding
 import com.posco.posco_store.databinding.ItemLoadingBinding
-import com.posco.posco_store.ui.main.view.DetailActivity
-
 import kotlinx.android.synthetic.main.item_layout.view.*
+
 
 import javax.inject.Inject
 import kotlin.collections.ArrayList
 
 class MainAdapter @Inject constructor(
-) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>(), Filterable {
 
     private val VIEW_TYPE_ITEM = 0
     private val VIEW_TYPE_LOADING = 1
     private var apps: ArrayList<App> = ArrayList()
     var appFilterList: ArrayList<App> = ArrayList()
     private var files: ArrayList<FileInfoDto> = ArrayList()
+    private var mShowLoading = false
+
 
 
     inner class DataViewHolder(itemView: ItemLayoutBinding) :
         RecyclerView.ViewHolder(itemView.root) {
+
         fun bind(app: App) {
             itemView.textViewUserName.text = app.appName
             val fileInfo = app.iconFileInfoDto
@@ -65,50 +65,43 @@ class MainAdapter @Inject constructor(
     }
 
     override fun getItemViewType(position: Int): Int {
-        return when (appFilterList[position].appName) {
-            " " -> VIEW_TYPE_LOADING
-            else -> VIEW_TYPE_ITEM
+        if(position < appFilterList.size){
+            return VIEW_TYPE_ITEM
         }
+        return  VIEW_TYPE_LOADING
 
     }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return when (viewType) {
+         when (viewType) {
             VIEW_TYPE_ITEM -> {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ItemLayoutBinding.inflate(layoutInflater, parent, false)
-                DataViewHolder(binding)
+                return DataViewHolder(binding)
             }
             else -> {
                 val layoutInflater = LayoutInflater.from(parent.context)
                 val binding = ItemLoadingBinding.inflate(layoutInflater, parent, false)
-                LoadingViewHolder(binding)
+                return LoadingViewHolder(binding)
             }
+
+
+
         }
     }
 
-    override fun getItemCount(): Int = appFilterList.size
+    override fun getItemCount(): Int = appFilterList.size + if(mShowLoading) {1} else{0}
 
     //user data List binding
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        if (holder is DataViewHolder) {
+        if (holder is MainAdapter.DataViewHolder) {
             holder.bind(appFilterList.get(position))
         }
-        //holder.bind(appFilterList.get(position))
 
-        //클릭시 user detail page로 이동
-//        holder.itemView.setOnClickListener {
-//            val bundle = Bundle().apply {
-//                putSerializable("selected_item", appFilterList[position])
-//            }
-//            val intent = Intent(holder.itemView.context, DetailActivity::class.java)
-//            Log.d("이거 왜 안됑", "??")
-//            startActivity(holder.itemView.context, intent, bundle)
-//        }
     }
 
-    fun getFilter(): Filter {
+    override fun getFilter(): Filter {
         return object : Filter() {
             override fun performFiltering(constraint: CharSequence): FilterResults {
                 val charString = constraint.toString()
@@ -133,24 +126,24 @@ class MainAdapter @Inject constructor(
             override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
                 appFilterList =
                     results?.values as ArrayList<App> /* = java.util.ArrayList<com.posco.posco_store.data.model.App> */
+                notifyDataSetChanged()
             }
         }
 
     }
 
-
-    fun deleteLoading() {
-        apps.removeAt(apps.lastIndex)
-        appFilterList.removeAt(appFilterList.lastIndex)
+    fun hideLoading(){
+        mShowLoading = false
+        notifyItemRemoved(itemCount)
     }
-
+    fun showLoading(){
+        mShowLoading = true
+        notifyItemInserted(itemCount+1)
+    }
 
     fun addData(app: List<App>) {
         apps.addAll(app)
         appFilterList.addAll(app)
-        apps.add(App(" ", " "))
-        appFilterList.add(App(" ", " "))
-
 
     }
 
--- app/src/main/java/com/posco/posco_store/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/posco/posco_store/ui/main/view/MainActivity.kt
@@ -6,17 +6,14 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.view.View
-import android.widget.AbsListView
 import android.widget.Toast
 import androidx.activity.viewModels
-import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.kakao.sdk.common.KakaoSdk.type
 import com.posco.posco_store.data.model.App
 import com.posco.posco_store.databinding.ActivityMainBinding
 import com.posco.posco_store.ui.main.adapter.MainAdapter
@@ -24,7 +21,6 @@ import com.posco.posco_store.ui.main.viewmodel.MainViewModel
 import com.posco.posco_store.utils.Status
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.serialization.descriptors.buildSerialDescriptor
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -32,6 +28,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     var adapter: MainAdapter = MainAdapter()
     private var index: Int = 10
+    var isLoading = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -77,16 +74,24 @@ class MainActivity : AppCompatActivity() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
                 val lastVisibleItemPosition = (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition() // 화면에 보이는 마지막 아이템의 position
-                val itemTotalCount = recyclerView.adapter!!.itemCount - 1 // 어댑터에 등록된 아이템의 총 개수 -1
+                val visibleItemCount = (recyclerView.layoutManager as LinearLayoutManager?)!!.childCount
+                val itemTotalCount = recyclerView.adapter!!.itemCount  // 어댑터에 등록된 아이템의 총 개수 -1
+                val firstVisibleItemPosition = (recyclerView.layoutManager as LinearLayoutManager?)!!.findFirstVisibleItemPosition() //첫번째 보이는것
 
                 // 스크롤이 끝에 도달했는지 확인
-                if (!binding.recyclerView.canScrollHorizontally(1) && lastVisibleItemPosition == itemTotalCount) {
-                    adapter.deleteLoading()
+             //   if ( lastVisibleItemPosition == itemTotalCount) {
+                if(!isLoading) {
+                    adapter.showLoading()
+                    Log.i("여기가","되는가")
+                    if ((visibleItemCount + firstVisibleItemPosition) >= itemTotalCount) {
 
-                    Log.i("왜 안됑?", index.toString())
+                        Log.i("왜 안됑?", index.toString())
 
-                    mainViewModel.getAllApp(++index)
-                    setupAPICall()
+                        index += 10
+                        mainViewModel.getAllApp(index)
+                       // adapter.notifyDataSetChanged()
+                        setupAPICall()
+                    }
                 }
             }
         })
@@ -111,6 +116,7 @@ class MainActivity : AppCompatActivity() {
         mainViewModel.getAllApp(index).observe(this, Observer {
             when (it.status) {
                 Status.SUCCESS -> {
+                    adapter.hideLoading()
                     progressBar.visibility = View.GONE
                     it.data?.let { usersData -> renderList(usersData) }
                     recyclerView.visibility = View.VISIBLE
