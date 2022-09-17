0a6268705a0d4fd6e5c20b85d154421b91844332
hannahf97 <46432606+hannahf97@users.noreply.github.com>
2022-09-18 00:58:52 +0900

    [ADD] loading image 수정

--- app/src/main/java/com/posco/posco_store/ui/main/view/SplashActivity.kt
+++ app/src/main/java/com/posco/posco_store/ui/main/view/SplashActivity.kt
@@ -12,7 +12,6 @@ class SplashActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         val handler = Handler()
 
         handler.postDelayed({
